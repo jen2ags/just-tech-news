29a26884d9b78216954571fb824a93678ad8fbbd
jennj <jennjenn93@gmail.com>
2022-04-26 22:48:20 -0500

    creating table

---
+++ models/User.js
@@ -0,0 +1,51 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+//create our User model
+class User extends Model {}
+
+//define table columns and configuration
+User.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unidue: true,
+            validate: {
+                isEmail: true
+            }
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [4]
+            }
+        }
+    },
+    {
+        //table configuration options go here (https://sequelize.org/v5/manual/models-definition.html#configuration))
+        //pass in our imported sequelize connection (to direct connection to our database
+        sequelize,
+        //don't automatically create createdAt/UpdatedAt timestamp fields
+        timestamps: false,
+        //don't pluralize name of database table
+        freezeTableName: true,
+        //use underscores instead of camel-casing (ie. `comment_text` and not `commentText`)
+        underscored: true,
+        //make it so our model name stays lowercase in the database
+        modelName: 'user'
+    }
+);
+
+module.exports = User;
