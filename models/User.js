17c94ae0c1a61968e746e14b5d89a01482a6186f
jennj <jennjenn93@gmail.com>
2022-04-27 19:32:07 -0500

    adding new table

--- models/User.js
+++ models/User.js
@@ -20,7 +20,7 @@ User.init(
         email: {
             type: DataTypes.STRING,
             allowNull: false,
-            unidue: true,
+            unique: true,
             validate: {
                 isEmail: true
             }
